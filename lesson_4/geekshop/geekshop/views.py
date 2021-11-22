6c2894ee7a132db80836a631d40fd077ed6eaed2
Konstantin <konstantinkail349@gmail.com>
2021-11-22 15:46:28 +0300

    lesson_4

---
+++ lesson_4/geekshop/geekshop/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+from mainapp.models import Product
+
+
+def main(request):
+    title = 'главная'
+    links_page = [
+        {'href': 'main', 'name': 'домой'},
+        {'href': 'products:index', 'name': 'товары'},
+        {'href': 'contacts', 'name': 'контакты'},
+    ]
+    products = Product.objects.all()[:4]
+    context = {
+        'title': title,
+        'links_page': links_page,
+        'products': products,
+    }
+    return render(request, 'geekshop/index.html', context=context)
+
+
+def contacts(request):
+    title = 'контакты'
+    links_page = [
+        {'href': 'main', 'name': 'домой'},
+        {'href': 'products:index', 'name': 'товары'},
+        {'href': 'contacts', 'name': 'контакты'},
+    ]
+    context = {
+        'title': title,
+        'links_page': links_page,
+    }
+    return render(request, 'geekshop/contact.html', context=context)
+
